de1a0dfe3928d99c606c1d0c1c27e91791fc4445
Erick Chandra <erickchandra.1@gmail.com>
2016-04-11 20:53:40 +0700

    Added users_model.

---
+++ I3P/application/models/users_model.php
@@ -0,0 +1,43 @@
+<?php
+	class Users_model extends CI_Model {
+		function __construct() {
+			parent::__construct();
+			$this->load->database();
+		}
+
+		public function get_all_with_id($id) {
+			$this->db->select('id, username, email, full_name, tel_no, type');
+			$query = $this->db->get_where('users', array('id' => $id));
+		}
+
+		public function insert_new_user($username, $email, $password, $full_name, $tel_no, $type) {
+			$insert_data = array(
+					'username' => $username,
+					'email' => $email,
+					'password' => $password,
+					'full_name' => $full_name,
+					'tel_no' => $tel_no,
+					'type' => $type
+				);
+			$this->db->insert('users', $insert_data);
+		}
+
+		public function update_tel_no($id, $new_tel_no) {
+			$insert_data = array(
+					'telno' => $new_tel_no
+				);
+
+			$this->db->where('id', $id);
+			$this->db->update('users', $insert_data);
+		}
+
+		public function change_password($id, $new_password) {
+			$insert_data = array(
+					'password' => $new_password
+				);
+
+			$this->db->where('id', $id);
+			$this->db->update('users', $insert_data);
+		}
+	}
+?>
