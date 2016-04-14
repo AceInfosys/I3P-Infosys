576e5edd1125a8ca3adad08a04442e9f55ddedc1
Erick Chandra <erickchandra.1@gmail.com>
2016-04-14 15:33:11 +0700

    Added header and footer view templates and registration view.

---
+++ I3P/application/views/register.php
@@ -0,0 +1,34 @@
+        <div class="container">
+            <div class="panel panel-default panel-reg">
+                <div class="panel-body">
+                    <div class="panelhead">Pendaftaran Pengguna Baru</div>
+                    <form role="form">
+                        <div class="form-group">
+                            <label class="labelreg" for="namalengkap"> Nama Lengkap</label>
+                            <input type="text" class="form-control" id="namalengkap" placeholder="Nama Lengkap">
+                        </div>
+                        <div class="form-group">
+                            <label class="labelreg" for="usrname"> Username</label>
+                            <input type="text" class="form-control" id="usrname" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <label class="labelreg" for="psw"> Password</label>
+                            <input type="text" class="form-control" id="psw" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label class="labelreg" for="email"> Email</label>
+                            <input type="text" class="form-control" id="email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label class="labelreg" for="notelp"> Nomor Telepon</label>
+                            <input type="text" class="form-control" id="notelp" placeholder="Nomor Telepon">
+                        </div>
+                        <div class="form-group">
+                            <label class="labelreg" for="alamat"> Alamat</label>
+                            <input type="text" class="form-control" id="alamat" placeholder="Alamat ">
+                        </div>
+                        <button type="submit" class="btn btnregister"> Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
---
+++ I3P/application/views/template/header.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        <meta name="description" content="Using for Information System task">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/style.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/bootstrap.min.css">
+        <script src="<?php echo base_url(); ?>/public/js/jquery.min.js"></script>
+        <script src="<?php echo base_url(); ?>/public/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="container-header">
+                    <a class="navbar-brand" href="index.html"><img class="logonav" src="<?php echo base_url(); ?>/public/img/logozen.png"></a>
+                </div>
+            </div>
+        </nav>
