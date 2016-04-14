7d728ca059a563316cf9c139690a2d2f7f743fff
Erick Chandra <erickchandra.1@gmail.com>
2016-04-14 15:34:20 +0700

    Modify autoload config and register controller.

--- I3P/application/controllers/Register.php
+++ I3P/application/controllers/Register.php
@@ -1,7 +1,10 @@
 <?php
 	class Register extends CI_Controller {
 		public function index() {
-			echo 'Register Test';
+			// echo 'Register Test';
+			$this->load->view('template/header');
+			$this->load->view('register');
+			$this->load->view('template/footer');
 		}
 	}
 ?>
