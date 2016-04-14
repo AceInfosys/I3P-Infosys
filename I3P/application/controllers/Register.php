5972e0303f67be7b5f7b6edfa7e9f28daec39778
Erick Chandra <erickchandra.1@gmail.com>
2016-04-14 20:00:47 +0700

    User registration successful connected.

--- I3P/application/controllers/Register.php
+++ I3P/application/controllers/Register.php
@@ -1,10 +1,37 @@
 <?php
 	class Register extends CI_Controller {
+		public function __construct() {
+			parent::__construct();
+			$this->load->model('users_model');
+			$this->load->model('customer_users_model');
+			$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+		}
+
 		public function index() {
 			// echo 'Register Test';
-			$this->load->view('template/header');
+			$this->load->view('template/header', array('title' => 'Register - I3P ZEN'));
 			$this->load->view('register');
 			$this->load->view('template/footer');
+		}
+
+		public function new_registration() {
+			$this->form_validation->set_rules('full_name', 'Full Name', 'required|xss_clean');
+			$this->form_validation->set_rules('username', 'Username', 'trim|required|is_unique[users.username]|xss_clean');
+			$this->form_validation->set_rules('password', 'Password', 'required|xss_clean');
+			$this->form_validation->set_rules('confirmation_password', 'Confirmation Password', 'required|matches[password]|xss_clean');
+			$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]|xss_clean');
+			$this->form_validation->set_rules('tel_no', 'Nomor Telepon', 'trim|required|xss_clean');
+
+			if ($this->form_validation->run() == FALSE) {
+				$this->load->view('template/header');
+				$this->load->view('register');
+				$this->load->view('template/footer');
+			}
+			else {
+				$clean = $this->security->xss_clean($this->input->post(NULL, TRUE));
+				$this->users_model->insert_new_user($clean['username'], $clean['email'], $clean['password'], $clean['full_name'], $clean['tel_no'], $clean['type']);
+				echo 'Success';
+			}
 		}
 	}
 ?>
--- I3P/application/views/register.php
+++ I3P/application/views/register.php
@@ -1,7 +1,7 @@
         <nav class="navbar">
             <div class="container-fluid">
                 <div class="container-header">
-                    <a class="navbar-brand" href="index.html"><img class="logonav" src="<?php echo base_url(); ?>/public/img/logozen.png"></a>
+                    <a class="navbar-brand" href="index.html"><img class="logonav" src="<?php echo base_url(); ?>public/img/logozen.png"></a>
                 </div>
             </div>
         </nav>
@@ -9,33 +9,114 @@
             <div class="panel panel-default panel-reg">
                 <div class="panel-body">
                     <div class="panelhead">Pendaftaran Pengguna Baru</div>
-                    <form role="form">
+                    <!-- <form role="form"> -->
+                    <?php echo form_open('register/new_registration'); ?>
                         <div class="form-group">
                             <label class="labelreg" for="namalengkap"> Nama Lengkap</label>
-                            <input type="text" class="form-control" id="namalengkap" placeholder="Nama Lengkap">
+                            <!-- <input type="text" class="form-control" id="namalengkap" placeholder="Nama Lengkap"> -->
+                            <?php
+                                $data = array(
+                                        'class' => 'form-control',
+                                        'name' => 'full_name',
+                                        'id' => 'full_name',
+                                        'maxlength' => '100',
+                                        'placeholder' => 'Nama Lengkap'
+                                    );
+                                echo form_input($data);
+                            ?>
+                            <?php echo form_error('full_name'); ?>
                         </div>
                         <div class="form-group">
                             <label class="labelreg" for="usrname"> Username</label>
-                            <input type="text" class="form-control" id="usrname" placeholder="Username">
+                            <!-- <input type="text" class="form-control" id="usrname" placeholder="Username"> -->
+                            <?php
+                                $data = array(
+                                        'class' => 'form-control',
+                                        'name' => 'username',
+                                        'id' => 'username',
+                                        'maxlength' => '100',
+                                        'placeholder' => 'Username'
+                                    );
+                                echo form_input($data);
+                            ?>
+                            <?php echo form_error('username'); ?>
                         </div>
                         <div class="form-group">
                             <label class="labelreg" for="psw"> Password</label>
-                            <input type="text" class="form-control" id="psw" placeholder="Password">
+                            <!-- <input type="text" class="form-control" id="psw" placeholder="Password"> -->
+                            <?php
+                                $data = array(
+                                        'class' => 'form-control',
+                                        'name' => 'password',
+                                        'id' => 'password',
+                                        'maxlength' => '100',
+                                        'placeholder' => 'Password'
+                                    );
+                                echo form_password($data);
+                            ?>
+                            <?php echo form_error('password'); ?>
+                        </div>
+                        <div class="form-group">
+                            <label class="labelreg" for="psw"> Confirm Password</label>
+                            <!-- <input type="text" class="form-control" id="psw" placeholder="Password"> -->
+                            <?php
+                                $data = array(
+                                        'class' => 'form-control',
+                                        'name' => 'confirmation_password',
+                                        'id' => 'confirmation_password',
+                                        'maxlength' => '100',
+                                        'placeholder' => 'Confirm Password'
+                                    );
+                                echo form_password($data);
+                            ?><?php echo form_error('confirmation_password'); ?>
                         </div>
                         <div class="form-group">
                             <label class="labelreg" for="email"> Email</label>
-                            <input type="text" class="form-control" id="email" placeholder="Email">
+                            <!-- <input type="text" class="form-control" id="email" placeholder="Email"> -->
+                            <?php
+                                $data = array(
+                                        'type' => 'email',
+                                        'class' => 'form-control',
+                                        'name' => 'email',
+                                        'id' => 'email',
+                                        'maxlength' => '100',
+                                        'placeholder' => 'Email'
+                                    );
+                                echo form_input($data);
+                            ?>
+                            <?php echo form_error('email'); ?>
                         </div>
                         <div class="form-group">
                             <label class="labelreg" for="notelp"> Nomor Telepon</label>
-                            <input type="text" class="form-control" id="notelp" placeholder="Nomor Telepon">
+                            <!-- <input type="text" class="form-control" id="notelp" placeholder="Nomor Telepon"> -->
+                            <?php
+                                $data = array(
+                                        'class' => 'form-control',
+                                        'name' => 'tel_no',
+                                        'id' => 'tel_no',
+                                        'maxlength' => '100',
+                                        'placeholder' => 'Nomor Telepon'
+                                    );
+                                echo form_input($data);
+                            ?>
+                            <?php echo form_error('tel_no'); ?>
                         </div>
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                             <label class="labelreg" for="alamat"> Alamat</label>
                             <input type="text" class="form-control" id="alamat" placeholder="Alamat ">
+                        </div> -->
+                        <div class="form-group">
+                            <label class="labelreg" for="type">Tipe Akun</label>
+                            <div class="form-control">
+                                <input type="radio" name="type" value="1" <?php echo set_radio('type', '1', TRUE); ?> />
+                                <span style="padding-left: 5px; padding-right: 18px;">Pelanggan</span>
+                                <input type="radio" name="type" value="2" <?php echo set_radio('type', '2'); ?> />
+                                <span style="padding-left: 5px; padding-right: 18px;">
+                                Pelamar Kerja</span>
+                            </div>
                         </div>
-                        <button type="submit" class="btn btnregister"> Register</button>
-                    </form>
+                        <button type="submit" class="btn btnregister">Register</button>
+                    <?php echo form_close(); ?>
                 </div>
             </div>
         </div>
